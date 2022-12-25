bd87662923cb7027011d6136136ecea219c5dfc5
joker69216 <hamaraad883@gmail.com>
2022-12-25 13:54:22 +0300

    simple routing in next

---
+++ pages/news/index.js
@@ -0,0 +1,10 @@
+import Link from "next/Link";
+
+export default function index() {
+  return (
+    <>
+      <div>index</div>
+      <Link href='/news/details' > import Link from 'next/Link'</Link>
+    </>
+  );
+}
