0108db93f0accf44192afdfdfd386be3c1e67fac
joker69216 <hamaraad883@gmail.com>
2023-01-09 11:55:34 +0300

    add nodejs connection with mongo db

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,9 +1,26 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
   if (req.methode === 'POST') {
     const data = req.body;
     const { title, image, address, description } = data;
 
     // fetch data here
+    const client = await MongoClient.connect(
+      'mongodb+srv://hama:zaovGHK9dy9mrzPn@cluster0.byhcoaq.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+
+    const db = client.db();
+
+    const meetups = db.collection('meetups');
+
+    const result = await meetups.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'data has been added successfully' });
   }
 };
 
