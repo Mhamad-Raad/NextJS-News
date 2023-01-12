5d35f62d6ad97a966abb13b8165b1aa3ef4bab44
joker69216 <hamaraad883@gmail.com>
2023-01-12 11:38:21 +0300

    delete unnecessary elements

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,7 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <main>
         <Component {...pageProps} />
-      </main>
     </Layout>
   );
 }
